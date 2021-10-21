7293b374706891c66a8c06a14fe903f21a4181f8
Wolf1989 <pdv8989@mail.ru>
2021-10-21 15:30:36 +0400

    homeworks

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -1,7 +1,7 @@
 """first_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,14 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from app.views import home_view
-
-
 urlpatterns = [
-    path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
-    # path('current_time/', time_view, name='time'),
-    # path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
 ]
